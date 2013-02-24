e44dc0e4a52ff7655b0ddcb612e9e503013b5d6d
jacques-finalist <jacques@finalist.com>
2013-02-24 12:23:32 +0100

    fixed error with random

--- scala/src/test/scala/org/agilegrenoble/objectcalisthenics/GildedRoseTest.scala
+++ scala/src/test/scala/org/agilegrenoble/objectcalisthenics/GildedRoseTest.scala
@@ -11,8 +11,9 @@ import scala.math.Ordered
 
 object GildedRoseTest {
 
-  private val Max_Backstage_Sellin = 30
-  private val Max_Quality = 50
+  private val MaxBackstageSellin = 30
+  private val MaxQuality = 50
+  
   private val Expected_Item_Names = List(
     "+5 Dexterity Vest",
     "Aged Brie",
@@ -74,11 +75,11 @@ class GildedRoseTest extends FlatSpec with ShouldMatchers with BeforeAndAfter {
   }
 
   it should "have repeated value of containsOnlybackstage passes" in {
-	val newItems=aBunchOfBackstagePasses().toList
-    gildedRose.items=newItems
+    val newItems = aBunchOfBackstagePasses().toList
+    gildedRose.items = newItems
     repeatUpdateQuality(11)
 
-	val items=gildedRose.items
+    val items = gildedRose.items
     val qualities = items.map(_.quality)
     println(qualities)
     qualities should containOnly(List(30, 48, 45, 0, 11, 0, 0, 0, 36, 15, 33, 50, 50, 27, 0, 26, 42,
@@ -86,23 +87,28 @@ class GildedRoseTest extends FlatSpec with ShouldMatchers with BeforeAndAfter {
       33, 0, 0, 37, 0, 43, 0, 0, 45, 50, 22, 43, 0, 30, 14, 44, 50, 0, 17, 0, 17, 50, 16, 50, 19, 44,
       0, 0, 37, 34, 0, 0, 0, 50, 0, 29, 40, 50, 50, 47, 0, 0, 47, 0, 26, 11, 26, 16, 0, 50, 0, 0, 0,
       35, 0, 50))
+    
     val sellIns = items.map(_.sellIn)
-    sellIns should containOnly(List(1, 18, -7, 6, 8, 16, -8, 6, 4, -9, 4, -2, -9, 8, 14, 7, 9, -9, 12,
-      2, -9, 12, -7, 5, -6, -1, -11, 2, -4, 9, 4, 15, 13, -4, -4, 12, -7, 9, -4, -7, 4, 0, 11, 6, -8,
-      3, 17, 12, 9, -4, 13, -5, 10, 4, 14, 6, 14, 1, -5, -3, 15, 11, -2, -6, -2, 2, -11, 17, 10, 18,
-      2, 14, -8, -5, 1, -1, 0, 14, 7, 9, -3, 2, -9, -1, -6, 5, -5, 8, -5, 11, 3, 17, 17, 13, 5, 11, 16,
-      -3, 6, 11))
+    sellIns should containOnly(List(
+        1, 18, -7, 6, 8, 16, -8, 6, 4, -9, 4, -2, -9, 8, 14, 7, 9, -9, 12, 2, -9, 12, -7, 5, -6,
+        -1, -11, 2, -4, 9,4, 15, 13, -4, -4, 12, -7, 9, -4, -7, 4, 0, 11, 6, -8, 3, 17, 12, 9, -4, 
+        13, -5, 10, 4, 14, 6, 14, 1, -5, -3, 15, 11, -2, -6, -2, 2, -11, 17, 10, 18, 2, 14, -8, -5, 1, -1, 0, 
+        14, 7, 9, -3, 2, -9, -1, -6, 5, -5, 8, -5, 11, 3, 17, 17, 13, 5, 11, 16, -3, 6, 11))
   }
 
   private def repeatUpdateQuality(times: Int) = for (i <- 0 until times) gildedRose.updateQuality()
 
   private def aBunchOfBackstagePasses() = (0 until 100).map(_ => aRandomBackstagePass)
 
-  private def randomSellIn() = rand.nextInt(Max_Backstage_Sellin)
+  private def randomSellIn() = rand.nextInt(MaxBackstageSellin)
 
-  private def randomQuality() = rand.nextInt(Max_Quality)
+  private def randomQuality() = rand.nextInt(MaxQuality)
 
-  private def aRandomBackstagePass() = Item("Backstage passes to a TAFKAL80ETC concert", randomSellIn(), randomQuality())
+  private def aRandomBackstagePass() = {
+    val quality = randomQuality()
+    val sellIn = randomSellIn()
+    Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality)
+  }
 
   def containOnly[A](right: Seq[A]) =
     new Matcher[Seq[A]] {
