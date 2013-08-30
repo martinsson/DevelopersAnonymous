6e33d7b1391a4319dd4855d750b43ccd7324e977
Johan Martinsson <martinsson.johan@gmail.com>
2013-08-30 20:05:10 +0300

    ready for part 2

--- java/src/main/java/org/joyofcoding/objectcalisthenics/Item.java
+++ java/src/main/java/org/joyofcoding/objectcalisthenics/Item.java
@@ -30,4 +30,54 @@ public class Item {
 	public int getQuality() {
 		return quality;
 	}
+
+    void update() {
+        if (!getName().equals("Aged Brie") && !getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (getQuality() > 0) {
+                if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
+                    setQuality(getQuality() - 1);
+                }
+            }
+        } else {
+            if (getQuality() < 50) {
+                setQuality(getQuality() + 1);
+    
+                if (getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (getSellIn() < 11) {
+                        if (getQuality() < 50) {
+                            setQuality(getQuality() + 1);
+                        }
+                    }
+    
+                    if (getSellIn() < 6) {
+                        if (getQuality() < 50) {
+                            setQuality(getQuality() + 1);
+                        }
+                    }
+                }
+            }
+        }
+    
+        if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
+            setSellIn(getSellIn() - 1);
+        }
+    
+        if (getSellIn() < 0) {
+            if (!getName().equals("Aged Brie")) {
+                if (!getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (getQuality() > 0) {
+                        if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
+                            setQuality(getQuality() - 1);
+                        }
+                    }
+                } else {
+                    setQuality(getQuality() - getQuality());
+                }
+            } else {
+                if (getQuality() < 50) {
+                    setQuality(getQuality() + 1);
+                }
+            }
+        }
+    }
 }
