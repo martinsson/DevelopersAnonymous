5f72f80270d86cca2efd5f8e0b3d60734f0d28c6
Johan Martinsson <martinsson.johan@gmail.com>
2013-10-14 01:40:57 +0200

    cleaned up conditional logic

--- java/src/main/java/org/joyofcoding/objectcalisthenics/Item.java
+++ java/src/main/java/org/joyofcoding/objectcalisthenics/Item.java
@@ -1,83 +1,83 @@
 package org.joyofcoding.objectcalisthenics;
 
 public class Item {
-	private String name;
-	private int sellIn;
-	private int quality;
+    private String name;
+    private int sellIn;
+    private int quality;
 
-	public Item(String name, int sellIn, int quality) {
-		this.name = name;
-		this.quality = quality;
-		this.sellIn = sellIn;
-	}
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.quality = quality;
+        this.sellIn = sellIn;
+    }
 
-	public void setSellIn(int sellIn) {
-		this.sellIn = sellIn;
-	}
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
 
-	public void setQuality(int quality) {
-		this.quality = quality;
-	}
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public int getSellIn() {
-		return sellIn;
-	}
+    public int getSellIn() {
+        return sellIn;
+    }
 
-	public int getQuality() {
-		return quality;
-	}
+    public int getQuality() {
+        return quality;
+    }
 
     void update() {
-        if (!getName().equals("Aged Brie") && !getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (getQuality() > 0) {
-                if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
-                    setQuality(getQuality() - 1);
-                }
+        if (getName().equals("Aged Brie")) {
+            increaseQuality();
+            decreaseSellIn();
+            if (getSellIn() < 0) {
+                increaseQuality();
             }
-        } else {
+        } else if (getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (getQuality() < 50) {
-                setQuality(getQuality() + 1);
-    
-                if (getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (getSellIn() < 11) {
-                        if (getQuality() < 50) {
-                            setQuality(getQuality() + 1);
-                        }
-                    }
-    
-                    if (getSellIn() < 6) {
-                        if (getQuality() < 50) {
-                            setQuality(getQuality() + 1);
-                        }
-                    }
+                increaseQuality();
+
+                if (getSellIn() < 11) {
+                    increaseQuality();
+                }
+
+                if (getSellIn() < 6) {
+                    increaseQuality();
                 }
             }
-        }
-    
-        if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
-            setSellIn(getSellIn() - 1);
-        }
-    
-        if (getSellIn() < 0) {
-            if (!getName().equals("Aged Brie")) {
-                if (!getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (getQuality() > 0) {
-                        if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
-                            setQuality(getQuality() - 1);
-                        }
-                    }
-                } else {
-                    setQuality(getQuality() - getQuality());
-                }
-            } else {
-                if (getQuality() < 50) {
-                    setQuality(getQuality() + 1);
-                }
+            decreaseSellIn();
+            if (getSellIn() < 0) {
+                setQuality(getQuality() - getQuality());
             }
+        } else if (getName().equals("Sulfuras, Hand of Ragnaros")) {
+        } else {
+            decreaseQuality();
+            decreaseSellIn();
+            if (getSellIn() < 0) {
+                decreaseQuality();
+            }
+        }
+
+    }
+
+    private void decreaseSellIn() {
+        setSellIn(getSellIn() - 1);
+    }
+
+    private void decreaseQuality() {
+        if (getQuality() > 0) {
+            setQuality(getQuality() - 1);
+        }
+    }
+
+    private void increaseQuality() {
+        if (getQuality() < 50) {
+            setQuality(getQuality() + 1);
         }
     }
 }
